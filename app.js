9be071e8f8226e4a7cf2f6df80b5c7648f8c808b
City7g <City7gor@gmail.com>
2024-09-03 14:18:06 +0300

    Add prisma with user table

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 import cryptoRoute from './routes/crypto.js'
 
 const app = express()
 const port = 8000
+const prisma = new PrismaClient()
 
 app.use('/crypto', cryptoRoute)
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
+app.get('/', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
 })
 
 app.listen(port, () => {
